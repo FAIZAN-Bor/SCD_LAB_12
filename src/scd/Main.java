b954b88d664426ad57fadbb1eeb643c4fe89b16c
FAIZAN-Bor <faizanafzaal868@gmail.com>
2024-11-15 22:34:25 +0500

    Pushh

--- src/scd/Main.java
+++ src/scd/Main.java
@@ -1,10 +1,13 @@
-public class Main {
-public static void main(String[] args) {
-System.out.println("Welcome to the application!");
-greetUser();
-}
+package scd;
 
-public static void greetUser() {
-System.out.println("Hello, User!");
-}
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to the application!");
+	greetUser();
+	}
+
+	public static void greetUser() {
+		System.out.println("Hello, User!");
+	}
 }
